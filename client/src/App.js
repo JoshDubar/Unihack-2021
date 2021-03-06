4cde5cef46810bca89c5706120d9d72156f00db4
Andy Lin <andylinnz79@gmail.com>
2021-03-06 12:04:58 +1100

    invite form

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm/LoginForm";
 import SignupForm from "./components/SignupForm/SignupForm";
+import InviteForm from "./components/InviteForm/InviteForm";
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         {/* <Route path="/" component={LoginForm} /> */}
         <Route path="/login" component={LoginForm} />
         <Route path="/signup" component={SignupForm} />
+        <Route path="/invite/:id" component={InviteForm} />
       </div>
     </Router>
   );
